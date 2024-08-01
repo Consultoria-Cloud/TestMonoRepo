702f7c553e4b4cb6ce6be7ff51047f729e9e59b2
André Salata <andreluizsalata@gmail.com>
2024-08-01 11:53:54 -0300

    Ajustando libs

--- src/TestLambda1/lambda_function.py
+++ src/TestLambda1/lambda_function.py
@@ -3,9 +3,8 @@ from src.libs.lib1.lib1 import lib1
 
 def lambda_handler(event, context):
     # TODO implement
+    lib1 ()
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda 1! Version v1.0.0')
     }
-
-lib1 ()
