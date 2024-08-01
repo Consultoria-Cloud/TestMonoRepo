5ae14ce57f977ba8d27b15c7d861638c99d0a740
André Salata <andreluizsalata@gmail.com>
2024-08-01 10:18:32 -0300

    Criando Lib1 e Lambda3

--- src/Lib1/lib1.py
+++ src/Lib1/lib1.py
@@ -1,10 +1,8 @@
 import json
 
-def lambda_handler(event, context):
+def lib1 (event, context):
     # TODO implement
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda 1! Version v1.0.0')
     }
-
-lib1 (event, context)
--- src/TestLambda3/lambda_function.py
+++ src/TestLambda3/lambda_function.py
@@ -4,7 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda 1! Version v1.0.0')
+        'body': json.dumps('Hello from Lambda 2! Version v2.0.0')
     }
-
-lib1 (event, context)
